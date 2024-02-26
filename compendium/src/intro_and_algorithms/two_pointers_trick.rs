0b4836bdb254bc83d577b6836c7b279b0a75d60c
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-02-26 12:31:45 +0100

    Added Number of Ways

--- compendium/src/intro_and_algorithms/two_pointers_trick.rs
+++ compendium/src/intro_and_algorithms/two_pointers_trick.rs
@@ -50,8 +50,63 @@ where
     (best_i, best_j)
 }
 
+/// Given an array of n elements, count the number of ways of splitting it in three parts
+/// such that the sum of the three parts is equal.
+///
+/// # Arguments
+///
+/// * `a`: the input vector
+///
+/// returns: (i32)
+/// \theta(n)
+pub fn numbers_of_ways(a: Vec<i32>) -> i32 {
+    let mut target = a.iter().sum();
+    if target % 3 != 0 {
+        return 0;
+    }
+
+    target = target / 3;
+
+    let mut sum = 0;
+    let mut i = 0;
+    while sum != target {
+        sum = sum + a[i];
+        i = i + 1;
+    }
+
+    sum = 0;
+    let mut j = a.len() - 1;
+    while sum != target {
+        sum = sum + a[j];
+        j = j - 1;
+    }
+
+    sum = 0;
+    let mut result = 0;
+    for k in i..=j {
+        sum = sum + a[k];
+        if sum == target {
+            result = result + 1;
+        }
+    }
+
+    result
+}
+
 #[test]
 pub fn test_k_good() {
     let a = vec![1, 1, 2, 3, 3, 4, 4, 4, 1, 3, 2, 3];
     assert_eq!(longest_k_good_segments(a, 3), (3, 9));
+}
+
+#[test]
+pub fn test_numbers_of_ways() {
+    let mut a = vec![1, 1, 1, 1, 1, 1];
+    assert_eq!(numbers_of_ways(a), 1);
+    a = vec![1, 5, 3, 3, 0, 0, 6];
+    assert_eq!(numbers_of_ways(a), 3);
+    a = vec![1, 2, 3, 0, 3];
+    assert_eq!(numbers_of_ways(a), 2);
+    a = vec![1, 2, 3, 4];
+    assert_eq!(numbers_of_ways(a), 0);
 }
