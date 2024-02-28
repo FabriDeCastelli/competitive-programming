eb2830c51f2a50febab1e570be938132811df92f
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-02-28 18:21:40 +0100

    Segment Trees: Dynamic Range Minimum Queries with Occurrences.

--- compendium/src/segment_trees/dynamic_range_minimum.rs
+++ compendium/src/segment_trees/dynamic_range_minimum.rs
@@ -1,20 +1,18 @@
+use crate::segment_trees::segment_tree::SegmentTree;
 use std::cmp::min;
 use std::fmt::Debug;
 use std::ops::Add;
-use crate::segment_trees::segment_tree::SegmentTree;
-
-// Assume SegmentTree is defined somewhere
 
 struct DynamicRMQ<T>
-    where
-        T: Default + Copy + Clone + Debug + Add<Output = T> + PartialOrd,
+where
+    T: Default + Copy + Clone + Debug + Add<Output = T> + PartialOrd,
 {
     segment_tree: SegmentTree<T, fn(T, T) -> T>,
 }
 
 impl<T> DynamicRMQ<T>
-    where
-        T: Default + Copy + Clone + Debug + Add<Output = T> + PartialOrd + Ord,
+where
+    T: Default + Copy + Clone + Debug + Add<Output = T> + PartialOrd + Ord,
 {
     pub fn from_vec(v: Vec<T>) -> Self {
         Self {
@@ -22,23 +20,42 @@ impl<T> DynamicRMQ<T>
         }
     }
 
-    pub fn update(&mut self, i: usize, v: T) {
+    pub fn add(&mut self, i: usize, v: T) {
         self.segment_tree.update(i, v);
     }
 
-    pub fn query(&self, l: usize, r: usize) -> Option<T> {
+    pub fn rmq(&self, l: usize, r: usize) -> Option<T> {
         self.segment_tree.query(l, r)
     }
-
 }
+
+/// Dynamic Range Minimum Queries with Occurrences.
+/// Simply define the combine and propagate functions in the following way:
+
+pub fn min_and_occurrences<T>(a: (T, T), b: (T, T)) -> (T, T)
+where
+    T: Default + Ord + Add,
+{
+    return if a.0 == b.0 {
+        (a.0, a.1 + b.1)
+    } else if a.0 < b.0 {
+        a
+    } else {
+        b
+    };
+}
+
+/// The propagate function is the same as the combine function. To use the problem just preprocess
+/// the array to have couples value, position and build the segment tree on top of it
+/// with the combine and propagate functions.
 
 #[test]
 pub fn test_dynamic_rmq() {
     let v = vec![1, 2, 3, 4, 5];
     let mut dynamic_rmq = DynamicRMQ::from_vec(v);
-    assert_eq!(dynamic_rmq.query(0, 4), Some(1));
-    dynamic_rmq.update(0, 10);
-    assert_eq!(dynamic_rmq.query(0, 4), Some(2));
-    dynamic_rmq.update(0, 1);
-    assert_eq!(dynamic_rmq.query(0, 4), Some(2));
+    assert_eq!(dynamic_rmq.rmq(0, 4), Some(1));
+    dynamic_rmq.add(0, 10);
+    assert_eq!(dynamic_rmq.rmq(0, 4), Some(2));
+    dynamic_rmq.add(0, 1);
+    assert_eq!(dynamic_rmq.rmq(0, 4), Some(2));
 }
