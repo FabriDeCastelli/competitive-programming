b2c483033d387f28fcc3e96b057b80f4d503a075
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-02-28 17:24:18 +0100

    Segment Trees: Update and Range Query.

--- compendium/src/segment_trees/segment_tree.rs
+++ compendium/src/segment_trees/segment_tree.rs
@@ -1,23 +1,26 @@
 use std::fmt::Debug;
 
-struct SegmentTree<T, F>
+struct SegmentTree<T, F, U>
 where
     T: Default + Copy + Clone + Debug,
     F: Fn(T, T) -> T,
+    U: Fn(T, T) -> T,
 {
     length: usize,
     tree: Vec<T>,
     lazy: Vec<T>,
     propagate: F,
     combine: F,
+    update: U,
 }
 
-impl<T, F> SegmentTree<T, F>
+impl<T, F, U> SegmentTree<T, F, U>
 where
     T: Default + Copy + Clone + Debug,
     F: Fn(T, T) -> T,
+    U: Fn(T, T) -> T,
 {
-    pub fn from_vec(v: Vec<T>, propagate: F, combine: F) -> Self {
+    pub fn from_vec(v: Vec<T>, propagate: F, combine: F, update: U) -> Self {
         let length = v.len();
         let lazy = vec![T::default(); 4 * length];
         let tree = vec![T::default(); 4 * length];
@@ -27,6 +30,7 @@ where
             lazy,
             propagate,
             combine,
+            update,
         };
         segment_tree.build(v.as_slice(), 0, length - 1, 0);
         segment_tree
@@ -48,6 +52,26 @@ where
         self.tree[current] = (self.combine)(self.tree[left_child], self.tree[right_child]);
     }
 
+    pub fn update(&mut self, i: usize, v: T) {
+        self.update_rec(i, v, 0, self.length - 1, 0)
+    }
+
+    fn update_rec(&mut self, i: usize, v: T, l: usize, r: usize, current: usize) {
+        if l == r {
+            self.tree[l] = (self.update)(self.tree[l], v);
+            return;
+        }
+
+        let mid = (l + r) / 2;
+        if i <= mid {
+            self.update_rec(i, v, l, mid, Self::left_child(current));
+        } else {
+            self.update_rec(i, v, mid + 1, r, Self::right_child(current));
+        }
+
+        self.tree[current] = (self.update)(self.tree[current], v);
+    }
+
     pub fn print(&self) {
         self.print_recursive(0, 0, self.length - 1);
     }
@@ -65,6 +89,46 @@ where
         }
     }
 
+    pub fn query(&self, ql: usize, qr: usize) -> Option<T> {
+        assert!(ql <= qr);
+        self.query_recursive(ql, qr, 0, self.length - 1, 0)
+    }
+
+    fn query_recursive(
+        &self,
+        ql: usize,
+        qr: usize,
+        l: usize,
+        r: usize,
+        current: usize,
+    ) -> Option<T> {
+        if Self::no_overlap(ql, qr, l, r) {
+            return None;
+        }
+        if Self::total_overlap(ql, qr, l, r) {
+            return Some(self.tree[current]);
+        }
+
+        let mid = (l + r) / 2;
+        let left = self.query_recursive(ql, qr, l, mid, Self::left_child(current));
+        let right = self.query_recursive(ql, qr, mid + 1, r, Self::right_child(current));
+
+        match (left, right) {
+            (Some(l), Some(r)) => Some((self.propagate)(l, r)),
+            (Some(l), None) => Some(l),
+            (None, Some(r)) => Some(r),
+            _ => None,
+        }
+    }
+
+    fn no_overlap(ql: usize, qr: usize, l: usize, r: usize) -> bool {
+        qr < l || r < ql
+    }
+
+    fn total_overlap(ql: usize, qr: usize, l: usize, r: usize) -> bool {
+        ql <= l && r <= qr
+    }
+
     fn left_child(index: usize) -> usize {
         2 * index + 1
     }
@@ -72,4 +136,13 @@ where
     fn right_child(index: usize) -> usize {
         2 * index + 2
     }
+}
+
+#[test]
+fn test_segment_tree() {
+    let v = vec![1, 2, 3, 4];
+    let sum = |a, b| a + b;
+    let mut segment_tree = SegmentTree::from_vec(v, sum, sum, sum);
+    segment_tree.update(1, 3);
+    assert_eq!(segment_tree.query(0, 3), Some(13));
 }
